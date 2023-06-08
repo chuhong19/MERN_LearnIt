234b0e9bf591d5ca4faaffb7f9cb7129ee5f3e70
Huynh Gia Bao <liveofdeathyesorno@gmail.com>
2023-06-08 23:10:49 +0700

    Check laptop

--- server/index.js
+++ server/index.js
@@ -25,6 +25,6 @@ app.use(cors())
 app.use('/api/auth', authRouter)
 app.use('/api/posts', postRouter)
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log('Server listening on port', PORT));
--- server/models/User.js
+++ server/models/User.js
@@ -2,19 +2,19 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    username: {
-        type: String,
-        require: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        require: true,
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now()
-    }
+  username: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
 });
 
-module.exports = mongoose.model('users', UserSchema)
+module.exports = mongoose.model('users', UserSchema);
